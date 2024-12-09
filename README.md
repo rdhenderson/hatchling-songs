64ccd3d4b4c987c9b3495547a30450996cfb2694
Reid Henderson <reid.henderson@gmail.com>
2024-12-09 15:47:57 -0500

    added script for cleaning file names and generating song list json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hatchling-songs
+
+Add a teacher specific directory to teachers directory and upload mp3 songs there.
+Run `node ./scripts/update_file_and_gen_song_list.js [teacher-name]` script to remove spaces from titles and create
+a song-list.json file in the teacher directory. (requires having node installed on machine)
--- scripts/update_file_and_gen_song_list.js
+++ scripts/update_file_and_gen_song_list.js
@@ -1,26 +1,28 @@
-const { dir } = require("console");
 const fs = require("fs");
 
-function generateSongArray(dir) {
-  const files = fs.readdirSync(dir);
+function generateSongArray(directory) {
+  const files = fs.readdirSync(`./teachers/${directory}`);
   const songs = files
     .filter((file) => !file.endsWith(".json"))
     .map((file) => {
-      const [title] = url.replace(/_/g, " ").split(".");
+      const [title] = file.replace(/_/g, " ").split(".");
       return { title, url: file };
     });
   return songs;
 }
 
-function updateFileNames(dir) {
+function updateFileNames(directory) {
   // Read the contents of the current directory synchronously
-  const files = fs.readdirSync(dir);
+  const files = fs.readdirSync(`./teachers/${directory}`);
   files
     .filter((file) => !file.endsWith(".json"))
     .forEach((file) => {
       if (file.includes(" ")) {
         const newFile = file.replace(/ /g, "_");
-        fs.renameSync(`./${dir}/${file}`, `./${dir}/${newFile}`);
+        fs.renameSync(
+          `./teachers/${directory}/${file}`,
+          `./teachers/${directory}/${newFile}`
+        );
       }
     });
 }
@@ -31,7 +33,7 @@ if (require.main === module) {
   updateFileNames(dirToUpdate);
   const songList = generateSongArray(dirToUpdate);
   fs.writeFileSync(
-    `./${dirToUpdate}/song-list.json`,
+    `./teachers/${dirToUpdate}/song-list.json`,
     JSON.stringify(songList, null, 2)
   );
   console.log(JSON.stringify(songList, null, 2));
